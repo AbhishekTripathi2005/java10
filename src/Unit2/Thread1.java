40d63a84de782884347a31b29ae8866234e00a56
Abhishek_tripathi <tripathiabhishek1808@gmail.com>
2025-09-25 09:34:56 +0530

    unit2

---
+++ src/Unit2/Thread1.java
@@ -0,0 +1,10 @@
+package Unit2;
+
+public class Thread1 extends Thread{
+    public void run(){
+        for(int i=0;i<11;i++){
+            System.out.println("Thread 1 "+i);
+
+        }
+    }
+}
