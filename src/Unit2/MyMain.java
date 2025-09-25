218d52d93402c88e614d4b795db5802e18d99ead
Abhishek_tripathi <tripathiabhishek1808@gmail.com>
2025-09-25 10:48:50 +0530

    unit2

--- src/Unit2/MyMain.java
+++ src/Unit2/MyMain.java
@@ -4,5 +4,14 @@ public class MyMain {
     public static void main(String[] args) {
         Thread1 th1 = new Thread1();
         th1.start();
+        System.out.println(th1.getPriority());
+        th1.setPriority(10);
+        System.out.println(th1.getPriority());
+        System.out.println(th1.getName());
+        th1.setName("My Thread 1");
+        System.out.println(th1.getName());
+        Thread2 th2 = new Thread2();
+        th2.start();
+        th2.setPriority(Thread.MAX_PRIORITY);
     }
 }
