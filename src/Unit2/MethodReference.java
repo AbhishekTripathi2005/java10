b55309ef93010085015c155212bd827f9c208919
Abhishek_tripathi <tripathiabhishek1808@gmail.com>
2025-10-15 09:58:30 +0530

    Lambda

---
+++ src/Unit2/MethodReference.java
@@ -0,0 +1,11 @@
+package Unit2;
+
+public class MethodReference {
+//    public static void saySomething() {
+//        System.out.println("Hello Static Method");
+//    }
+    public void saysomething(){
+        System.out.println("Hello World");
+    }
+
+}
---
+++ src/Unit2/myStaticMethod.java
@@ -0,0 +1,9 @@
+package Unit2;
+
+public class myStaticMethod {
+    public static void main(String[] args) {
+//        Sayable sayable = MethodReference::saySomething;
+//        sayable.say();
+
+    }
+}
